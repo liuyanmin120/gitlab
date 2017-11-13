03c3454286fac7247724012bddf0edeb4df0b993
liuym <806585962@qq.com>
2017-11-13 16:20:38 +0800

    gitlab

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo rm -rf ./config
+sudo rm -rf ./data
+sudo rm -rf ./logs
---
+++ insgitlab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo docker pull gitlab/gitlab-ce:latest
+mkdir config
+mkdir logs
+mkdir data
+sudo docker stop gitlab
+# -d 后台
+sudo docker run -d \
+--hostname gitlabhostname \
+--name gitlab \
+--publish 443:443 \
+--publish 9888:9888 \
+--publish 9999:9999 \
+--publish 2200:22 \
+--publish 8080:80 \
+--restart always \
+--volume ~/gitlab/config:/etc/gitlab \
+--volume ~/gitlab/logs:/var/log/gitlab \
+--volume ~/gitlab/data:/var/opt/gitlab \
+gitlab/gitlab-ce:latest
